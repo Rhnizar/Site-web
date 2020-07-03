95c2af022da4cfd2bbbb969f6ff0bcbafd870140
Rida Rhnizar <ridarhnizar1337@gmail.com>
2020-07-04 00:50:27 +0100

    ofppt

---
+++ siteo.js
@@ -0,0 +1,18 @@
+ function enregistrer()
+ {
+	 alert("Les information enregistrer");
+ }
+ 
+ function Accepter()
+ {
+	 alert("Les informations envoyé");
+ }
+
+ function Refuser()
+ {
+	 alert("Les information Annuler");
+ }
+ if(checkbox==true)
+ {
+	 alert("yes");
+ }
